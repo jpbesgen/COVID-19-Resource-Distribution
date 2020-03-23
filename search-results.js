8f7d6f06d2b7df1096ff6884c4c0d2f385741fc7
Samarth Goel <sgoel9@berkeley.edu>
2020-03-22 21:51:01 -0500

    Added Isotope Functionality and Rerouted

--- search-results.js
+++ search-results.js
@@ -8,7 +8,7 @@ const data = new Promise(res => res({
         quantity: "70",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         type: "mask",
-        category: "ppe",
+        category: "PPE",
         maker: {
             name: "bob",
             rating: "4"
@@ -20,7 +20,7 @@ const data = new Promise(res => res({
         quantity: "2",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         type: "ventilator",
-        category: "medical parts",
+        category: "medical-parts",
         maker: {
             name: "dove",
             rating: "4"
@@ -32,41 +32,71 @@ const data = new Promise(res => res({
         quantity: "2",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         type: "ventilator",
-        category: "medical parts",
+        category: "medical-parts",
         maker: {
             name: "joe",
             rating: "4"
         }
       },
+      {
+        image: "./img/team3.png",
+        name: "Hand Sanitizor",
+        quantity: "15 Liters",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+        type: "other",
+        category: "other",
+        maker: {
+            name: "bill",
+            rating: "5"
+        }
+      },
     ]
   }));
-  
+
   // This is important don't forget it!
   // it makes sure the document is loaded
   $(document).ready(() => {
     
+    const $grid = $('#grid').isotope({
+      itemSelector: '.grid-item'
+    });
+
+    $('.filters-button-group').on( 'click', 'button', function() {
+      const filterValue = $( this ).attr('data-filter');
+      console.log(filterValue);
+      console.log($grid);
+      // use filterFn if matches value
+      // filterValue = filterFns[ filterValue ] || filterValue;
+      $grid.isotope({ filter: filterValue });
+    });
+
     fillGrid = async () => {
       // This is where the api call goes
       const response = await data;
       
       response.data.forEach(gridItem => {
         // this is where jQuery steps in  
-        $('#grid').append(
-            `<div class="${gridItem.type} ${gridItem.category} grid-item media">
-                <img class="mr-3" src="${gridItem.image}" alt="Item" />
-                <div class="media-body">
-                    <h5 class="mt-0">${gridItem.name} - quantity: ${gridItem.quantity}</h5>
-                    ${gridItem.description}
-                </div>
-                <a href="./order-page.html">
-                    <button type="button" class="btn btn-outline-dark btn-lg order-button">
-                        Order
-                    </button>
-                </a>
-            <div>`
+        var $items = $(
+          `<div class="${gridItem.type} ${gridItem.category} grid-item media">
+          <img class="mr-3" src="${gridItem.image}" alt="Item" />
+          <div class="media-body">
+              <h5 class="mt-0">${gridItem.name} - quantity: ${gridItem.quantity}</h5>
+              ${gridItem.description}
+          </div>
+          <a href="./order-page.html">
+              <button type="button" class="btn btn-outline-dark btn-lg order-button">
+                  Order
+              </button>
+          </a>
+      <div>`
         );
+
+        // append items to grid
+        $grid.append( $items )
+            .isotope( 'appended', $items );
       })
+
     }
-    
+
     fillGrid()
   })
