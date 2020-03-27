31ce419714ef829130db9fbc1e72c3b27e165cf7
Alex Madrzyk <amadrzyk@slack-corp.com>
2020-03-27 01:29:01 -0700

    add login flow

---
+++ js/firebase_login.js
@@ -0,0 +1,33 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+	callbacks: {
+		signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+			// User successfully signed in.
+			// Return type determines whether we continue the redirect automatically
+			// or whether we leave that to developer to handle.
+			return true;
+		},
+		uiShown: function() {
+			// The widget is rendered.
+			// Hide the loader.
+			document.getElementById('loader').style.display = 'none';
+		}
+	},
+	// Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+	signInFlow: 'popup',
+	signInSuccessUrl: '/index.html',
+	signInOptions: [
+		// Leave the lines as is for the providers you want to offer your users.
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+	],
+	// Terms of service url.
+	tosUrl: 'https://www.google.com/',
+	// Privacy policy url.
+	privacyPolicyUrl: 'https://www.google.com/'
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
