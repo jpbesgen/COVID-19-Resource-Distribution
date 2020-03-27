c9a0a389845b1a3595088f03f6d4651fa8e403a2
Samarth Goel <sgoel9@berkeley.edu>
2020-03-26 22:36:27 -0500

    added modal functionality

--- search-results.js
+++ search-results.js
@@ -99,14 +99,38 @@ const data = new Promise(res => res({
           <div class="card ${gridItem.type} ${gridItem.category} grid-item">
             <img class="card-img-top" src="${gridItem.image}" alt="Card image cap">
             <div class="card-body">
-              <h5 class="card-title text-dark">${gridItem.name} - quantity: ${gridItem.quantity}</h5>
+              <h5 class="card-title text-dark">${gridItem.name}</h5>
               <p class="card-title text-dark">Quantity: ${gridItem.quantity}</p>
               <p class="card-text">${gridItem.description}</p>
               <hr style="border-top: 1px solid black;margin: 0;" />
               <ul class="list-group list-group-flush text-center">
+                <li class="list-group-item">
+                  <button class="btn btn-block" data-toggle="modal" data-target="#${gridItem.id}">See More</button>
+                </li>
                 <li class="list-group-item"><button class="btn btn-block">upvote</button></li>
                 <li class="list-group-item"><button class="btn btn-block">downvote</button></li>
               </ul>
+            </div>
+          </div>
+
+          <div class="modal fade" id="${gridItem.id}" tabindex="-1" role="dialog" aria-labelledby="${gridItem.id}ModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="${gridItem.id}ModalLabel">${gridItem.name}</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <img class="card-img-top" src="${gridItem.image}" alt="Card image cap">
+                  <p class="card-title text-dark">Quantity: ${gridItem.quantity}</p>
+                  ${gridItem.description}
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
             </div>
           </div>
           `
