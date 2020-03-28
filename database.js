d96c376582d1db0e6550fbb00291eb85039a7e66
Alex Madrzyk <amadrzyk@slack-corp.com>
2020-03-28 13:14:37 -0700

    add carousel

--- database.js
+++ database.js
@@ -52,76 +52,99 @@ function renderDesigns(designs) {
 
         if (gridItem.approved){
             $('#grid').append(
-            `
-            <div class="card ${gridItem.type} ${gridItem.category} grid-item">
-            <h5 class="card-header text-dark">${gridItem.name}</h5>
-                <img class="card-img-top" src="${gridItem.images[0].url}" alt="Item Attachment 0" />
-                <div class="card-body">
-                    <p class="card-text"><b>Category:</b> ${gridItem.category}</p>
-                    <p class="card-text item-description">${description}</p>
-                    <p class="card-text"><b>3D printer Required:</b> ${gridItem.printerRequired}</p>
-                    <p class="card-text"><b>Certified:</b> ${gridItem.certified}</p>
-                    <button class="btn btn-block card-text" data-toggle="modal" data-target="#${gridItem.id}">See More</button>
-
-                </div>
-                <div class="btn-group">
-                    <button onClick="upvote('${gridItem.id}')" class="btn">Upvote</button>
-                    <button onClick="downvote('${gridItem.id}')" class="btn">Downvote</button> 
-                </div>               
-            </div>
-
-           <div class="modal fade" id="${gridItem.id}" tabindex="-1" role="dialog" aria-labelledby="${gridItem.id}ModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="${gridItem.id}ModalLabel">${gridItem.name}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+                `
+                <div class="card ${gridItem.type} ${gridItem.category} grid-item">
+                <h5 class="card-header text-dark">${gridItem.name}</h5>
+                    <img class="card-img-top" src="${gridItem.images[0].url}" alt="Item Attachment 0" />
+                    <div class="card-body">
+                        <p class="card-text"><b>Category:</b> ${gridItem.category}</p>
+                        <p class="card-text item-description">${description}</p>
+                        <p class="card-text"><b>3D printer Required:</b> ${gridItem.printerRequired}</p>
+                        <p class="card-text"><b>Certified:</b> ${gridItem.certified}</p>
+                        <button class="btn btn-block card-text" data-toggle="modal" data-target="#${gridItem.id}">See More</button>
+    
                     </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-xs-12 col-6">
-                                <img class="modal-img" src="${gridItem.images[0].url}" alt="Modal item cap" />
-                            </div>
-                            <div class="col-xs-12 col-6">
-                                <div class="community">
-                                    <div class="votes">
-                                        <h3 class="community-title">Community Score</h3>
-                                        <p class="community-text">${gridItem.upvotes} Upvotes</p>
+                    <div class="btn-group">
+                        <button onClick="upvote('${gridItem.id}')" class="btn">Upvote</button>
+                        <button onClick="downvote('${gridItem.id}')" class="btn">Downvote</button> 
+                    </div>               
+                </div>
+    
+               <div class="modal fade" id="${gridItem.id}" tabindex="-1" role="dialog" aria-labelledby="${gridItem.id}ModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="${gridItem.id}ModalLabel">${gridItem.name}</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-xs-12 col-6">
+    <!--                                <img class="modal-img" src="" alt="Modal item cap" />-->
+                                    <!-- CAROUSEL -->
+                                    <div class="owl-carousel owl-theme">
+                                        <div class="carousel-image" style="background-image: url('${gridItem.images[0].url}');" />
+                                        <div class="carousel-image" style="background-image: url('https://via.placeholder.com/250x160');" />
+                                        <div class="carousel-image" style="background-image: url('https://via.placeholder.com/150x300');" />
                                     </div>
-                                    <div class="comments">
-                                        <h3 class="community-title">Comments</h3>
-                                        <small class="form-text text-muted">Username</small>
-                                        <p class="community-text">${gridItem.comments}</p>
-                                        <button class="btn" style="border:1px solid black">Make a Comment</button>
+                                    <!-- END CAROUSEL -->
+                                </div>
+                                <div class="col-xs-12 col-6">
+                                    <div class="community">
+                                        <div class="votes">
+                                            <h3 class="community-title">Community Score</h3>
+                                            <p class="community-text">${gridItem.upvotes} Upvotes</p>
+                                        </div>
+                                        <div class="comments">
+                                            <h3 class="community-title">Comments</h3>
+                                            <small class="form-text text-muted">Username</small>
+                                            <p class="community-text">${gridItem.comments}</p>
+                                            <button class="btn" style="border:1px solid black">Make a Comment</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <p class="modal-text"><b>Category</b><br />${gridItem.category}</p>
+                            <p class="modal-text"><b>Description</b><br />${gridItem.description}</p>
+                            <p class="modal-text"><b>3D Printer Required</b><br />${gridItem.printerRequired}</p>
+                            <p class="modal-text"><b>Certified</b><br /> ${gridItem.certified}</p>
+                            <p class="modal-text"><b>Difficulty Level</b><br /> ${gridItem.difficulty}</p>
+                            <p class="modal-text"><b>Credit</b><br /> ${gridItem.credit}</p>
+                            Links: ${links}
+                            Attachments: ${downloads}
                         </div>
-                        <p class="modal-text"><b>Category</b><br />${gridItem.category}</p>
-                        <p class="modal-text"><b>Description</b><br />${gridItem.description}</p>
-                        <p class="modal-text"><b>3D Printer Required</b><br />${gridItem.printerRequired}</p>
-                        <p class="modal-text"><b>Certified</b><br /> ${gridItem.certified}</p>
-                        <p class="modal-text"><b>Difficulty Level</b><br /> ${gridItem.difficulty}</p>
-                        <p class="modal-text"><b>Credit</b><br /> ${gridItem.credit}</p>
-                        Links: ${links}
-                        Attachments: ${downloads}
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
                     </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     </div>
                 </div>
-                </div>
-            </div>
             `
             );
+
             // append items to grid
             // $grid.append( $items )
                 // .isotope( 'appended', $items );
+
+            // enable carousel
+            $(".owl-carousel").owlCarousel({
+                items: 1,
+                margin: 10,
+                autoHeight: true,
+                nav: true,
+                loop: true,
+                lazyLoadEager: 1,
+                autoplayHoverPause: true,
+                navText: "<>",
+                autoplay: true,
+                autoplayTimeout: 5000,
+            });
         }
-        })
-    }
+      })
+    };
+
     fillGrid()
   })
 }
