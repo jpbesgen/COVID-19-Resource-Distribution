ce81229c6f09d0f43d8807332b7e6fa27326e584
Alex Madrzyk <amadrzyk@slack-corp.com>
2020-03-30 18:03:23 -0700

    add comment lib

--- database.js
+++ database.js
@@ -5,175 +5,192 @@ function renderDesigns(designs) {
     console.log(designs);
     $(document).ready(() => {
 
-    const $grid = $('#grid').isotope({
-      itemSelector: '.grid-item',
-      percentPosition: true,
-      masonry: {
-        columnWidth: 30
-      }
-    });
+        const $grid = $('#grid').isotope({
+          itemSelector: '.grid-item',
+          percentPosition: true,
+          masonry: {
+            columnWidth: 30
+          }
+        });
 
-    $('.dropdown-menu').on( 'click', 'button', function() {
-      const filterValue = $( this ).attr('data-filter');
-      console.log(filterValue);
-      console.log($grid);
-      // use filterFn if matches value
-      // filterValue = filterFns[ filterValue ] || filterValue;
-      $grid.isotope({ filter: filterValue });
-    });
+        $('.dropdown-menu').on( 'click', 'button', function() {
+          const filterValue = $( this ).attr('data-filter');
+          console.log(filterValue);
+          console.log($grid);
+          // use filterFn if matches value
+          // filterValue = filterFns[ filterValue ] || filterValue;
+          $grid.isotope({ filter: filterValue });
+        });
 
-    fillGrid = async () => {
-        let grid = document.getElementById("grid");
-        if(grid == null) return;
-        grid.innerHTML = "";
-        designs.forEach(gridItem => {
-        // this is where jQuery steps in  
-        // var $items = $(
-        let description = gridItem.description;
-        if (description.length > 140) {
-            description = description.substring(0, 141);
-            description +=  "...";
-        }
+        fillGrid = async () => {
+            let grid = document.getElementById("grid");
+            if(grid == null) return;
+            grid.innerHTML = "";
+            designs.forEach(gridItem => {
+            // this is where jQuery steps in
+            // var $items = $(
+            let description = gridItem.description;
+            if (description.length > 140) {
+                description = description.substring(0, 141);
+                description +=  "...";
+            }
 
-        // create downloadable links
-        let downloads = ``;
-        if(gridItem.attachments != null && gridItem.attachments.length > 0) {
-            gridItem.attachments.forEach((attachment) => {
-                downloads += `<a href="${attachment.url}" target="_blank" download> ${attachment.name}</a>`;
-                if(attachment != gridItem.attachments[gridItem.attachments.length - 1]) {
-                    downloads += `,`
-                }
-            });
-        }
-        
-        let links = ``;
-        if(gridItem.links != null && gridItem.links.length > 0) {
-            gridItem.links.forEach((link) => {
-                links += `<a href="${link}" target="_blank"> ${link}</a>`;
-                if(link != gridItem.links[gridItem.links.length - 1]) {
-                    links += `,`
-                }
-            })
-        }
+            // create downloadable links
+            let downloads = ``;
+            if(gridItem.attachments != null && gridItem.attachments.length > 0) {
+                gridItem.attachments.forEach((attachment) => {
+                    downloads += `<a href="${attachment.url}" target="_blank" download> ${attachment.name}</a>`;
+                    if(attachment != gridItem.attachments[gridItem.attachments.length - 1]) {
+                        downloads += `,`
+                    }
+                });
+            }
 
-        let comments = ``,
-            { retrievedComments } = gridItem;
-        if(retrievedComments != null && retrievedComments.length > 0) {
-            retrievedComments.forEach((comment) => {
-                comments += `<p class="modal-text" id="${comment.id}-comment">${comment.content} <b>from ${comment.author}</b></p>`;
-                if(comment != retrievedComments[retrievedComments.length - 1]) {
-                    comments += `<hr class="comment-divider"/>`;
-                }
-            });
-        }
+            let links = ``;
+            if(gridItem.links != null && gridItem.links.length > 0) {
+                gridItem.links.forEach((link) => {
+                    links += `<a href="${link}" target="_blank"> ${link}</a>`;
+                    if(link != gridItem.links[gridItem.links.length - 1]) {
+                        links += `,`
+                    }
+                })
+            }
 
-        
-        let addCommentDisplay = 
-        `
-            <input type="text" placeholder="Write a comment..." id="${gridItem.id}-comment-input"/>
-            <small class="form-text text-muted">from ${getUser() == null ? "" : getUser().displayName}</small>
-            <input class="btn" onClick="addComment('${gridItem.id}')" style="border:1px solid black" value="Make a Comment"/>
-        `;
+            let comments = ``,
+                { retrievedComments } = gridItem;
+            if(retrievedComments != null && retrievedComments.length > 0) {
+                retrievedComments.forEach((comment) => {
+                    comments += `<p class="modal-text" id="${comment.id}-comment">${comment.content} <b>from ${comment.author}</b></p>`;
+                    if(comment != retrievedComments[retrievedComments.length - 1]) {
+                        comments += `<hr class="comment-divider"/>`;
+                    }
+                });
+            }
 
-        if (gridItem.approved){
-            var $items = $(
-                `
-            <div class="grid-item card ${gridItem.type} ${gridItem.category} certified-${gridItem.certified} printer-${gridItem.printerRequired}" style="width: 18em;">
-            <h5 class="card-header text-dark">${gridItem.name}</h5>
-                <img class="card-img-top" src="${gridItem.images[0].url}" alt="Item Attachment 0" />
-                <div class="card-body">
-                    <p class="card-text"><b>Category:</b> ${gridItem.category}</p>
-                    <p class="card-text item-description">${description}</p>
-                    <p class="card-text"><b>3D printer Required:</b> ${gridItem.printerRequired}</p>
-                    <p class="card-text"><b>Certified:</b> ${gridItem.certified}</p>
-                    <button class="btn btn-block card-text" data-toggle="modal" data-target="#${gridItem.id}">See More</button>
-                </div>
-                <div class="btn-group">
-                    <button onClick="upvote('${gridItem.id}')" class="btn">Upvote</button>
-                    <button onClick="downvote('${gridItem.id}')" class="btn">Downvote</button> 
-                </div>               
-            </div>
-           <div class="modal fade" id="${gridItem.id}" tabindex="-1" role="dialog" aria-labelledby="${gridItem.id}ModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="${gridItem.id}ModalLabel">${gridItem.name}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-xs-12 col-6">
-<!--                                <img class="modal-img" src="${gridItem.images[0].url}" alt="Modal item cap" />-->
-                                <!-- CAROUSEL -->
-                                <div class="owl-carousel owl-theme">
-                                    <div class="carousel-image" style="background-image: url('${gridItem.images[0].url}');" />
-                                    <div class="carousel-image" style="background-image: url('https://via.placeholder.com/250x160');" />
-                                    <div class="carousel-image" style="background-image: url('https://via.placeholder.com/150x300');" />
-                                </div>
-                                <!-- END CAROUSEL -->
-                            </div>
-                            <div class="col-xs-12 col-6">
-                                <div class="community">
-                                    <div class="votes">
-                                        <h3 class="community-title">Community Score</h3>
-                                        <p class="community-text">${gridItem.upvotes} Upvotes</p>
-                                    </div>
-                                    <div class="comments">
-                                        <h3 class="community-title">Comments</h3>
-                                        <div class="comments-container">
-                                            <p class="community-text">${comments}</p>
-                                        </div>
-                                        ${getUser() == null ? "" : addCommentDisplay}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <p class="modal-text"><b>Category</b><br />${gridItem.category}</p>
-                        <p class="modal-text"><b>Description</b><br />${gridItem.description}</p>
-                        <p class="modal-text"><b>3D Printer Required</b><br />${gridItem.printerRequired}</p>
-                        <p class="modal-text"><b>Certified</b><br /> ${gridItem.certified}</p>
-                        <p class="modal-text"><b>Difficulty Level</b><br /> ${gridItem.difficulty}</p>
-                        <p class="modal-text"><b>Credit</b><br /> ${gridItem.credit}</p>
-                        Links: ${links}
-                        Attachments: ${downloads}
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-                </div>
-            </div>
+
+            let addCommentDisplay =
             `
-            );
-            console.log($items);
-            // append items to grid
-            // $grid.append( $items )
-                // .isotope( 'appended', $items );
+                <input type="text" placeholder="Write a comment..." id="${gridItem.id}-comment-input"/>
+                <small class="form-text text-muted">from ${getUser() == null ? "" : getUser().displayName}</small>
+                <input class="btn" onClick="addComment('${gridItem.id}')" style="border:1px solid black" value="Make a Comment"/>
+            `;
 
-            // enable carousel
-            $(".owl-carousel").owlCarousel({
-                items: 1,
-                margin: 10,
-                autoHeight: true,
-                nav: true,
-                loop: true,
-                lazyLoadEager: 1,
-                lazyLoad: false,
-                autoplayHoverPause: true,
-                navText: "<>",
-                autoplay: true,
-                autoplayTimeout: 5000,
-            });
+            if (gridItem.approved){
+                var $items = $(
+                    `
+                <div class="grid-item card ${gridItem.type} ${gridItem.category} certified-${gridItem.certified} printer-${gridItem.printerRequired}" style="width: 18em;">
+                <h5 class="card-header text-dark">${gridItem.name}</h5>
+                    <img class="card-img-top" src="${gridItem.images[0].url}" alt="Item Attachment 0" />
+                    <div class="card-body">
+                        <p class="card-text"><b>Category:</b> ${gridItem.category}</p>
+                        <p class="card-text item-description">${description}</p>
+                        <p class="card-text"><b>3D printer Required:</b> ${gridItem.printerRequired}</p>
+                        <p class="card-text"><b>Certified:</b> ${gridItem.certified}</p>
+                        <button class="btn btn-block card-text" data-toggle="modal" data-target="#${gridItem.id}">See More</button>
+                    </div>
+                    <div class="btn-group">
+                        <button onClick="upvote('${gridItem.id}')" class="btn">Upvote</button>
+                        <button onClick="downvote('${gridItem.id}')" class="btn">Downvote</button> 
+                    </div>               
+                </div>
+               <div class="modal fade" id="${gridItem.id}" tabindex="-1" role="dialog" aria-labelledby="${gridItem.id}ModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="${gridItem.id}ModalLabel">${gridItem.name}</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-xs-12 col-6">
+    <!--                                <img class="modal-img" src="${gridItem.images[0].url}" alt="Modal item cap" />-->
+                                    <!-- CAROUSEL -->
+                                    <div class="owl-carousel owl-theme">
+                                        <div class="carousel-image" style="background-image: url('${gridItem.images[0].url}');" />
+                                        <div class="carousel-image" style="background-image: url('https://via.placeholder.com/250x160');" />
+                                        <div class="carousel-image" style="background-image: url('https://via.placeholder.com/150x300');" />
+                                    </div>
+                                    <!-- END CAROUSEL -->
+                                </div>
+                                <div class="col-xs-12 col-6">
+                                    <div class="community">
+                                        <div class="votes">
+                                            <h3 class="community-title">Community Score</h3>
+                                            <p class="community-text">${gridItem.upvotes} Upvotes</p>
+                                        </div>
+                                        <div class="comments">
+                                            <h3 class="community-title">Comments</h3>
+                                            <div class="comments-container">
+                                                <p class="community-text">${comments}</p>
+                                            </div>
+                                            ${getUser() == null ? "" : addCommentDisplay}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="modal-text"><b>Category</b><br />${gridItem.category}</p>
+                            <p class="modal-text"><b>Description</b><br />${gridItem.description}</p>
+                            <p class="modal-text"><b>3D Printer Required</b><br />${gridItem.printerRequired}</p>
+                            <p class="modal-text"><b>Certified</b><br /> ${gridItem.certified}</p>
+                            <p class="modal-text"><b>Difficulty Level</b><br /> ${gridItem.difficulty}</p>
+                            <p class="modal-text"><b>Credit</b><br /> ${gridItem.credit}</p>
+                            Links: ${links}
+                            Attachments: ${downloads}
+                            <hr />
+                            <div id="comments-container"></div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                `
+                );
+                console.log($items);
+                // append items to grid
+                // $grid.append( $items )
+                    // .isotope( 'appended', $items );
 
-            $grid.append( $items )
-                .isotope( 'appended', $items );
-        }
-    })};
+                // enable carousel
+                $(".owl-carousel").owlCarousel({
+                    items: 1,
+                    margin: 10,
+                    autoHeight: true,
+                    nav: true,
+                    loop: true,
+                    lazyLoadEager: 1,
+                    lazyLoad: false,
+                    autoplayHoverPause: true,
+                    navText: "<>",
+                    autoplay: true,
+                    autoplayTimeout: 5000,
+                });
 
-    fillGrid()
-  })
+                $('#comments-container').comments({
+                    profilePictureURL: 'https://viima-app.s3.amazonaws.com/media/public/defaults/user-icon.png',
+                    getComments: function(success, error) {
+                        var commentsArray = [{
+                            id: 1,
+                            created: '2015-10-01',
+                            content: 'Lorem ipsum dolort sit amet',
+                            fullname: 'Simon Powell',
+                            upvote_count: 2,
+                            user_has_upvoted: false
+                        }];
+                        success(commentsArray);
+                    }
+                });
+
+                $grid.append( $items )
+                    .isotope( 'appended', $items );
+            }
+        })};
+
+        fillGrid()
+      })
 }
 
 // handleDesigns
