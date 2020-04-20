05997d33cfdc48b6db01fef0287d36e17ffea3f7
Dhruhin Kurli <dhruhin@gmail.com>
2020-04-19 17:28:13 -0700

    Track link names that are clicked
    * Add a name to link/attachment events

--- js/analytics.js
+++ js/analytics.js
@@ -29,11 +29,13 @@ function trackMakerSpaceClicks() {
     $('a.card-link').on({
       click: function() {
         let id = $(this).attr('design-id');
+        let name = $(this).text();
         let design = DBStore.getDesignsMap()[id];
         logEvent(ANALYTICS_EVENTS.CARD_LINK_CLICKED, {
           design_name: design.name,
           design_id: design.id,
           upvotes_at_time: design.upvotes,
+          name: name,
         });
       }
     });
@@ -41,11 +43,13 @@ function trackMakerSpaceClicks() {
     $('a.card-attachment').on({
       click: function() {
         let id = $(this).attr('design-id');
+        let name = $(this).text();
         let design = DBStore.getDesignsMap()[id];
         logEvent(ANALYTICS_EVENTS.CARD_ATTACHMENT_CLICKED, {
           design_name: design.name,
           design_id: design.id,
           upvotes_at_time: design.upvotes,
+          name: name,
         });
       }
     });
