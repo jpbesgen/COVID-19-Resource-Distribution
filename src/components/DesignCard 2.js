1d7e4e0f8f5f17ff2adbe436e7f1ba4fb7c3bd9d
Nik Yerasi <nyerasi@yahoo.com>
2020-04-23 11:35:47 -0700

    initial commit
    copied example card from react-bootstrap
    
    added example card to makerspace.js
    
    added certified label and tags to card component
    
    adjusted style, adding buttons
    
    added react multicarousel from npm
    
    merged makerspace changes, added styling
    
    card design pretty much donezo
    
    added props to DesignCard and helper methods
    
    added separate makerspacecarousel file
    
    added viewall and donate button
    
    rebased with develop-v2
    
    added react multicarousel from npm
    
    merged makerspace changes, added styling
    
    card design pretty much donezo
    
    added props to DesignCard and helper methods
    
    added separate makerspacecarousel file
    
    added viewall and donate button
    
    adding dependencies
    
    added example card to makerspace.js
    
    added certified label and tags to card component
    
    adjusted style, adding buttons
    
    added react multicarousel from npm
    
    merged makerspace changes, added styling
    
    card design pretty much donezo
    
    added separate makerspacecarousel file
    
    added viewall and donate button
    
    rebased with develop-v2
    
    added react multicarousel from npm
    
    merged makerspace changes, added styling
    
    card design pretty much donezo
    
    added separate makerspacecarousel file
    
    added viewall and donate button
    
    adding dependencies
    
    what have I done

---
+++ src/components/DesignCard 2.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Carousel from 'react-bootstrap/Carousel'
+
+// css copied from makerspace-carousel for now
+import '../css/design-card.css';
+
+import PlaceholderImage from '../img/doctormaskcolored.png';
+import UpvoteButtonImage from '../img/arrow-dropdown.png';
+import CheckmarkImage from '../img/check-mark.png';
+// props and state
+
+function VotingComponent(props) {
+  // props:
+  return (
+    <span id="makerspace-card-voting-container">
+      <Button variant="primary" id="makerspace-card-vote-button">
+        <img src={UpvoteButtonImage}/>
+      </Button>
+      <h1 id="makerspace-card-vote-count">{props.votes}</h1>
+      <Button variant="primary" id="makerspace-card-vote-button">
+        <img src={UpvoteButtonImage} id="makerspace-downvote-image"/>
+      </Button>
+    </span>
+  );
+}
+
+function CertifiedLabel(props) {
+  //props:
+  if (!props.showCertifiedLabel) {
+    return null
+  } else {
+    return (
+      <span id="makerspace-card-certified-label">
+        Certified
+        <img src={CheckmarkImage} id="makerspace-card-certified-image"/>
+      </span>
+    );
+  }
+}
+
+function DifficultyLabel(props) {
+  // props: difficulty (String)
+  // I think...
+
+  var color = "#6BA48C"
+  switch(props.difficulty) {
+    case "Easy":
+      color = "#9DDB8D";
+      break;
+    case "Med":
+      color = "#FFC773";
+      break;
+    case "Hard":
+      color = "#FF8888";
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <span id="makerspace-card-difficulty-label" style={{backgroundColor: color}}>
+      {props.difficulty}
+    </span>
+  );
+}
+
+function Tags(props) {
+  // props: tags (array of Strings)
+  let items = []
+  for (let i = 0; i < props.tags.length; i++) {
+    items.push(<span id="makerspace-card-tag">{props.tags[i]}</span>);
+  }
+  return (
+    // loop through list of tags and add to div as span
+    <div id="makerspace-card-tag-container">
+      {items}
+    </div>
+  );
+}
+
+class DesignCard extends React.Component {
+  /*
+  DesignCard this.props:
+  design_id
+  title
+  image
+  is_certified
+  difficulty
+  tags
+  description
+  upvote_count
+  */
+
+  constructor(props) {
+    super(props);
+    this.state = {hasUpvoted: false, hasDownvoted: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleDownvote = this.handleDownvote.bind(this);
+    this.handleUpvote = this.handleUpvote.bind(this);
+  }
+
+  truncateString(str, num) {
+    if (str.length <= num) {
+      return str;
+    }
+    return str.slice(0, num) + '...';
+  }
+
+  formatDescription(desciption) {
+    // description (String)
+    return this.truncateString(desciption, 90);
+  }
+
+  formatTitle(title) {
+    // title (String)
+    return this.truncateString(title, 25);
+  }
+
+  handleUpvote() {
+    // todo
+  }
+
+  handleDownvote() {
+    // todo
+  }
+
+  render() {
+    return (
+      <span>
+        <Card id="makerspace-card">
+          <div id="makerspace-card-image-container">
+          <Card.Img variant="top" src={this.props.image ? this.props.image : PlaceholderImage} id="makerspace-card-image"/>
+          <CertifiedLabel showCertifiedLabel={this.props.is_certified ? true : false}/>
+          <DifficultyLabel difficulty={this.props.difficulty ? this.props.difficulty : "Easy"}/>
+          </div>
+          <Card.Body>
+            <Card.Title id="makerspace-card-title">{this.props.title ? this.formatTitle(this.props.title) : ""}</Card.Title>
+            <Tags tags={this.props.tags ? this.props.tags : []}/>
+            <Card.Text id="makerspace-card-description">
+            {this.props.description ? this.formatDescription(this.props.description) : ""}
+            </Card.Text>
+          </Card.Body>
+          <VotingComponent votes={this.props.upvote_count ? this.props.upvote_count : 0}/>
+        </Card>
+      </span>
+    );
+  }
+}
+
+export default DesignCard;
---
+++ src/components/MakerspaceCarousel.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import Navbar from './Navbar';
+import DesignCard from './DesignCard.js';
+// import Carousel from 'react-bootstrap/Carousel'
+import PlaceholderImage from '../img/doctormaskcolored.png';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import '../css/makerspace-carousel.css';
+import { Link } from '@reach/router';
+import Button from 'react-bootstrap/Button';
+
+
+
+
+
+const MakerspaceCarousel = () => {
+    function setDonateButtonHover(e) {
+		e.target.style.color = '#3B628B';
+		e.target.style.background = 'transparent';
+	}
+
+	function unsetDonateButtonHover(e) {
+		e.target.style.color = 'white';
+		e.target.style.background = '#3B628B';
+    }
+    function setviewAllButtonHover(e) {
+		e.target.style.color = 'gray';
+		e.target.style.background = 'transparent';
+	}
+
+	function unsetviewAllButtonHover(e) {
+		e.target.style.color = 'silver';
+		e.target.style.background = 'transparent';
+	}
+    return (
+        <div id = "carousel-large-block">
+            <div>
+                <h1 id="makerspace-carousel-name">Masks</h1>
+                {/* <span id="makerspace-viewall-button"> */}
+                <Link to="/makerspace" id="viewall-button">
+                <Button id = "viewall-button"
+						onMouseEnter={setviewAllButtonHover}
+						onMouseLeave={unsetviewAllButtonHover}
+					>
+						<b>View All ></b>
+					</Button>
+				</Link>
+                <Link to="/hospitals">
+                    <Button id = "donate-button"
+						onMouseEnter={setDonateButtonHover}
+						onMouseLeave={unsetDonateButtonHover}
+					>
+						<b>Donate</b>
+					</Button>
+				</Link>
+
+                {/* </span> */}
+            </div>
+
+            <Carousel responsive={responsive}
+                infinite
+                renderButtonGroupOutside={true} >
+                    <div id = "design-div"><DesignCard
+                      title = "Some Mask"
+                      is_certified = {true}
+                      difficulty = "Med"
+                      tags = {["Fabric", "Elastic", "Filter", "Sewing Machine"]}
+                      description = "A surgical mask, also known as a procedure mask, medical mask or simply as a face mask, is intended to be worn by health professionals during surgery and during nursing to catch the bacteria shed in liquid droplets and aerosols from the wearer's mouth and nose."
+                      upvote_count = {34}
+                    />
+                    </div>
+                    <div id = "design-div" ><DesignCard/></div>
+                    <div id = "design-div" ><DesignCard/></div>
+                    <div id = "design-div" ><DesignCard/></div>
+                    <div id = "design-div" ><DesignCard/></div>
+            </Carousel>
+        </div>
+
+    );
+};
+
+const responsive = {
+	superLargeDesktop: {
+	  // the naming can be any, depends on you.
+	  breakpoint: { max: 4000, min: 3000 },
+	  items: 5
+	},
+	desktop: {
+	  breakpoint: { max: 3000, min: 1024 },
+	  items: 4
+	},
+	tablet: {
+	  breakpoint: { max: 1024, min: 464 },
+	  items: 2
+	},
+	mobile: {
+	  breakpoint: { max: 464, min: 0 },
+	  items: 1
+	}
+  };
+
+export default MakerspaceCarousel;
