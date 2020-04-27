f18645299452c131bbec87ad095bccedee587fdd
Shivam Patel <shivampatel@Shivams-MacBook-Air.local>
2020-04-26 23:21:38 -0400

    added separate makerspacecarousel file

--- src/components/Makerspace.js
+++ src/components/Makerspace.js
@@ -5,47 +5,21 @@ import DesignCard from './DesignCard.js';
 import PlaceholderImage from '../img/doctormaskcolored.png';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import MakerspaceCarousel from './MakerspaceCarousel'
 
 const Makerspace = () => {
 	return (
 		<div>
 			<Navbar />
 			<section id="makerspace-carousel">
+			{/* <DesignCard/> */}
 			</section>
-			<div style={{padding: '20px'}}>
-			<Carousel responsive={responsive}
-			infinite>
-  				<DesignCard/>
-  				<DesignCard/>
-					<DesignCard/>
-  				<DesignCard/>
-					<DesignCard/>
-  				<DesignCard/>
-			</Carousel>
-			</div>
+			<MakerspaceCarousel/>
 		</div>
 	);
 };
 
-const responsive = {
-	superLargeDesktop: {
-	  // the naming can be any, depends on you.
-	  breakpoint: { max: 4000, min: 3000 },
-	  items: 5
-	},
-	desktop: {
-	  breakpoint: { max: 3000, min: 1024 },
-	  items: 4
-	},
-	tablet: {
-	  breakpoint: { max: 1024, min: 464 },
-	  items: 2
-	},
-	mobile: {
-	  breakpoint: { max: 464, min: 0 },
-	  items: 1
-	}
-  };
+
 
 let style = {};
 
